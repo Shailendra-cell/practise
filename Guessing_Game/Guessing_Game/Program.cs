dd6ac8eb45aadd9072784c58dabf7c231db48e96
Shailendra-cell <80269393+Shailendra-cell@users.noreply.github.com>
2021-04-04 14:33:26 +0530

    Add files via upload

---
+++ Guessing_Game/Guessing_Game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string secretWord = "MissingWord";
+            string guess = "";
+            int guesscount = 0;
+            int guesslimit = 4;
+            bool outOfguess = false;
+
+            while (guess != secretWord && !outOfguess)
+            {
+                if(guesscount < guesslimit) 
+                {
+                    Console.Write("Enter guess:-");
+                    guess = Console.ReadLine();
+                    guesscount++;
+                }
+                else 
+                {
+                    outOfguess = true;
+                }
+            }
+            if(outOfguess)
+            {
+                Console.Write("opps better luck next time");
+            }
+            else
+            {
+                Console.Write("yeah you got it");
+            }
+        }
+    }
+}
